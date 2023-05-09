d632de59f02298a6b9a374e33d56e6b9316dc638
mark <djonte0@gmail.com>
2023-05-09 14:25:02 +0300

    added a working authentication

--- fitmax/fitmaxapp/urls.py
+++ fitmax/fitmaxapp/urls.py
@@ -5,5 +5,9 @@ from django.contrib.auth import views as auth_views
 app_name = 'fitMax'
 urlpatterns = [
     path('',views.index,name = 'index'),
-    path('pay/',views.pay,name = 'pay')
+    path('pay/',views.pay,name = 'pay'),
+    path('auth/login',views._login,name = 'login'),
+    path('auth/sign-up/',views.sign_up,name = 'sign_up'),
+    path('auth/logout',views.logout_view,name = 'logout'),
+    path('login-required/',views.required,name = 'login-required')
 ]
--- fitmax/fitmaxapp/views.py
+++ fitmax/fitmaxapp/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
+from .models import *
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -6,3 +14,49 @@ def index(request):
 
 def pay(request):
     return render(request,'fitmaxapp/pay.html',{})
+
+
+
+def _login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+
+    user = authenticate(request, username=email, password=password)
+    if user is not None:
+        login(request,user)
+        messages.success(request,'Welcome back {0},we are happy to see you again.'.format(user.first_name))
+        return HttpResponseRedirect(reverse('fitMax:index'))
+    else:
+        messages.warning(request,'Wrong credential combination,please check your credentials and try again.')
+        return HttpResponseRedirect(reverse('fitMax:index'))
+
+
+def sign_up(request):
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    email = request.POST['email']
+    password = request.POST['password']
+    cpass = request.POST['cpassword']
+
+    if password == cpass:
+        try:
+            user = User.objects.create_user(first_name = fname, last_name = lname, email = email,password = password,username=email)
+            user.save()
+            login(request,user)
+            messages.success(request,'Congratulations {0},You have been successfully registered'.format(fname))
+            return HttpResponseRedirect(reverse('fitMax:index'))
+        except:
+            messages.warning(request,'That username is already taken,please choose another name.')
+            return HttpResponseRedirect(reverse('fitMax:index'))
+    else:
+        messages.warning(request,'Passwords do not match,sign up failed')
+        return HttpResponseRedirect(reverse('fitMax:index'))
+        
+def logout_view(request):
+    messages.warning(request,'You have been successfully logged out.')
+    logout(request)
+    return HttpResponseRedirect(reverse('fitMax:index'))
+
+def required(request):
+    messages.warning(request,'Please log in to access this feature')
+    return HttpResponseRedirect(reverse('fitMax:index'))
